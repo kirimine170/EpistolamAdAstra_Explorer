12e4d351ae22fa6a547fd0eac13b97c5731ca4d4
kirimine170 <kirimine170@gmail.com>
2023-11-15 14:00:42 +0900

    📦 add vuetify
    なんかいい感じのUIフレームワークらしい

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
