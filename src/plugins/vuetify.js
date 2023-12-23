48839c3a436a2c987eb719c7af018350bf123f4c
kirimine170 <kirimine170@gmail.com>
2023-12-23 15:18:09 +0900

    🎨 add EAA-icon
    アイコンとしてSVGを追加しておいた

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,18 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 
 // Vuetify
 import { createVuetify } from 'vuetify'
+import Icon from '@/components/icons/EpistolamAdAstraIcon.vue'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    }
+  }
+})
